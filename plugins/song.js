8d0743db2b81b96e9b75badedb241018e9fcd15b
HMSer <95879255+HMser@users.noreply.github.com>
2022-01-22 12:08:56 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -1,15 +1,15 @@
 const Asena = require('../events');
-const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
-const fs = require('fs');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 
-Asena.addCommand({pattern: 'song', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
 
-    var r_text = new Array ();    
-r_text[0] = "\n*If It's not working* \n *Use .need*\n";
+Asena.addCommand({pattern: 'song', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-var i = Math.floor(1*Math.random())
+    var skl = await axios.get("https://i.imgur.com/gl6Ncpx.jpeg", { responseType: 'arraybuffer' })
 
-await message.sendMessage(r_text[i]);
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `\n*If It's not working* \n *Use .need*\n
+`}) 
 
 }));
