c6ba6f59d3b1d36bb6c37c104921556a755fd666
HMSer <95879255+HMser@users.noreply.github.com>
2022-01-27 18:06:13 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,7 +7,7 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/9OrgpKW.jpeg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.ibb.co/cvyPtps/QL.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Liza Mwol bot created by HM*
 *_Visit Website To Get Git_*
